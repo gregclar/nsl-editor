63189be6287f2c37356c85b5fa2125e49071246e
Greg Clarke <gregbclarke@gmail.com>
2019-12-23 17:38:32 +1100

    Configure relqtive url

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module V6021
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_controller.relative_url_root = "/nsl/hub"
   end
 end
