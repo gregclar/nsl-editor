ae0c86d7a7379cac2a2f1992b715d2087340d599
Greg Clarke <gregbclarke@gmail.com>
2020-04-21 10:06:05 +1000

    Added missing typeaheads

---
+++ app/javascript/packs/typeaheads/for_name/cultivar_parent.js
@@ -0,0 +1,42 @@
+// Used on name edit form.
+function setUpNameCultivarParentTypeahead() {
+    debug('setUpNameCultivarParentTypeahead');
+    if (typeof(nameParentSuggestionsForCultivar) != "undefined") {
+        $("#name-parent-typeahead").typeahead({highlight: true}, {
+            name: "preceding-name-id-parent",
+            displayKey: function(obj) {
+                return obj.value;
+            },
+            source: nameParentSuggestionsForCultivar.ttAdapter()})
+            .on('typeahead:opened', function($e,datum) {
+                debug('typeahead:opened'); })
+            .on('typeahead:selected', function($e,datum) {
+                debug('typeahead:selected');
+                $('#name_parent_id').val(datum.id) })
+            .on('typeahead:autocompleted', function($e,datum) {
+                debug('typeahead:autocompeted');
+                $('#name_parent_id').val(datum.id) })
+        ;
+    };
+}
+
+window.setUpNameCultivarParentTypeahead = setUpNameCultivarParentTypeahead;
+
+// Provides a way to inject the current name id into the URL.
+nameParentSuggestionsForCultivar = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {url: window.relative_url_root + '/suggestions/name/cultivar_parent?term=%QUERY',
+        replace: function(url,query) {
+            return window.relative_url_root + '/suggestions/name/cultivar_parent?' +
+                'name_id=' + $('#name-parent-typeahead').attr('data-name-id') + '&' +
+                'rank_id=' + $('#name_name_rank_id').val() + '&' +
+                'term=' + encodeURIComponent(query.replace(/\|.*/,''))
+        }
+    },
+    limit: 100
+});
+
+// kicks off the loading/processing of `local` and `prefetch`
+nameParentSuggestionsForCultivar.initialize();
+
---
+++ app/javascript/packs/typeaheads/for_name/hybrid_parent.js
@@ -0,0 +1,43 @@
+// Used on name edit form.
+function setUpNameHybridParentTypeahead() {
+    if (typeof(nameParentSuggestionsForHybrid) != "undefined") {
+        $("#name-parent-typeahead").typeahead({highlight: true}, {
+            name: "preceding-name-id-parent",
+            displayKey: function(obj) {
+                return obj.value;
+            },
+            source: nameParentSuggestionsForHybrid.ttAdapter()})
+            .on('typeahead:opened', function($e,datum) {
+                debug('typeahead:opened');
+            })
+            .on('typeahead:selected', function($e,datum) {
+                debug('typeahead:selected');
+                $('#name_parent_id').val(datum.id) })
+            .on('typeahead:autocompleted', function($e,datum) {
+                debug('typeahead:autocompeted');
+                $('#name_parent_id').val(datum.id) })
+        ;
+    };
+}
+
+window.setUpNameHybridParentTypeahead = setUpNameHybridParentTypeahead;
+
+
+// Provides a way to inject the current name id into the URL.
+nameParentSuggestionsForHybrid = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {url: window.relative_url_root + '/suggestions/name/hybrid_parent?term=%QUERY',
+        replace: function(url,query) {
+            return window.relative_url_root + '/suggestions/name/hybrid_parent?' +
+                'name_id=' + $('#name-parent-typeahead').attr('data-name-id') + '&' +
+                'rank_id=' + $('#name_name_rank_id').val() + '&' +
+                'term=' + encodeURIComponent(query.replace(/\|.*/,''))
+        }
+    },
+    limit: 100
+});
+
+// kicks off the loading/processing of `local` and `prefetch`
+nameParentSuggestionsForHybrid.initialize();
+
