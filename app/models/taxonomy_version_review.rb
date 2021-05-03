0ebf29bf462565efff6873bca48a5c50882983a9
Greg Clarke <gregbclarke@gmail.com>
2021-05-03 17:08:51 +1000

    NSL-3612: further work

--- app/models/taxonomy_version_review.rb
+++ app/models/taxonomy_version_review.rb
@@ -24,7 +24,7 @@ class TaxonomyVersionReview < ActiveRecord::Base
   #self.sequence_name = "nsl_global_seq"
   belongs_to :tree_version
   validates :name, presence: true
-  has_many :taxonomy_version_review_periods
+  has_many :periods, class_name: 'TaxonomyVersionReviewPeriod'
  
   # The table isn't in all schemas, so check it's there
   def self.exists?
