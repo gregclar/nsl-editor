8a6fb4c7feceb1d2d322fd87a54ea15986b9500d
Greg Clarke <gregbclarke@gmail.com>
2021-09-10 09:45:40 +1000

    Changing babel loose from false to true for more testing

--- babel.config.js
+++ babel.config.js
@@ -44,10 +44,10 @@ module.exports = function(api) {
       [
         '@babel/plugin-proposal-class-properties',
         {
-          loose: false
+          loose: true
         }
       ],
-      ["@babel/plugin-proposal-private-methods", { "loose": false }]
+      ["@babel/plugin-proposal-private-methods", { "loose": true }]
       [
         '@babel/plugin-proposal-object-rest-spread',
         {
