204053ca466b2652032684d43dd3d19c7798f0f3
Greg Clarke <gregbclarke@gmail.com>
2022-01-28 17:23:21 +1100

    Revert to jruby-9.3.1.0 because warbler fails to build war file with NoMethodError: undefined method new_ostruct_member for No value for 'public' found error

--- setup-dev-linux.sh
+++ setup-dev-linux.sh
@@ -44,7 +44,7 @@ JAVA_OPTS='-server -d64'
 
 # JRUBY
 echo "** setting up JRuby"
-JRUBY_VERSION=9.3.3.0
+JRUBY_VERSION=9.3.1.0
 TOOLS=build-tools
 JRUBY_ZIP=${TOOLS}/jruby-dist-${JRUBY_VERSION}-bin.zip
 JRUBY_HOME=$PWD/${TOOLS}/jruby-${JRUBY_VERSION}
