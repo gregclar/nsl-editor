45d0b2f12cb831614ff0080f5ed504f9c2657c2b
Greg Clarke <gregbclarke@gmail.com>
2023-02-21 16:26:12 +1100

    NSL-4434: Loader: Allow synonyms and misapplications to be created from existing synonyms and misapplications; make sequencing easier; correct positon of success message

--- app/models/loader/name.rb
+++ app/models/loader/name.rb
@@ -314,7 +314,10 @@ class Loader::Name < ActiveRecord::Base
   def self.create(params, username)
     loader_name = Loader::Name.new(params)
     loader_name.created_manually = true
-    loader_name.loader_batch_id = self.find(params[:parent_id]).loader_batch_id if loader_name.loader_batch_id.blank?
+    if loader_name.loader_batch_id.blank?
+      loader_name.loader_batch_id = self.find(params[:parent_id])
+                                        .loader_batch_id
+    end
     loader_name.doubtful = false
     loader_name.full_name = loader_name.simple_name
     if loader_name.save_with_username(username)
@@ -338,25 +341,24 @@ class Loader::Name < ActiveRecord::Base
     children.empty? && loader_name_matches.empty?
   end
 
-  def new_child
+  def new_child(base_seq = self.seq)
     loader_name = Loader::Name.new
     loader_name.parent_id = id
-    loader_name.simple_name = nil
-    loader_name.full_name = nil
+    loader_name.simple_name = loader_name.full_name = nil
     loader_name.family = family
-    loader_name.seq = seq + 1
+    loader_name.seq = base_seq + 1
     loader_name.created_manually = true
     loader_name
   end
 
-  def new_synonym
-    loader_name = new_child
+  def new_synonym(base_seq: self.seq)
+    loader_name = new_child(base_seq)
     loader_name.record_type = 'synonym'
     loader_name
   end
 
-  def new_misapp
-    loader_name = new_child
+  def new_misapp(base_seq: self.seq)
+    loader_name = new_child(base_seq)
     loader_name.record_type = 'misapplied'
     loader_name
   end
