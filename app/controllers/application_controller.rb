7f2ec76a97a4f4de466695cb0f5ad0c3959d2d28
Greg Clarke <gregbclarke@gmail.com>
2023-05-03 11:13:21 +1000

    NSL-4470: Loader Batches New Review form now submits correctly - fixed by explicitly adding 'data: {remote: true}' to the form_with call, same as fix for the edit form earlier today.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception  # from v4, needed in v6?
+  protect_from_forgery with: :exception
   before_action :set_debug,
                 :start_timer,
                 :check_system_broadcast,
