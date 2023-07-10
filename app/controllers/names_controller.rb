158634712a8cb1c074a6311bc4dba59375e7df0d
Greg Clarke <gregbclarke@gmail.com>
2023-07-11 08:51:34 +1000

    NSL-4561: minor refinement on previous to clear the code scan alert

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -21,7 +21,6 @@ require "open-uri"
 #   Names are central to the NSL.
 class NamesController < ApplicationController
   include OpenURI
-  protect_from_forgery
   # All text/html requests should go to the search page, except for rules.
   before_action :javascript_only, except: [:rules, :refresh_children]
   before_action :find_name,
