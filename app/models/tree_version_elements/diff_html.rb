2bf71c62060353bd0369c40765a17d2fa42c0ae3
Greg Clarke <gregbclarke@gmail.com>
2023-07-26 11:39:08 +1000

    NSL-4584: Code Quality: Change affecting Tree-change-difference display (alerts added for likely unused code models/tree_version_elements/diff_html.rb

--- app/models/tree_version_elements/diff_html.rb
+++ app/models/tree_version_elements/diff_html.rb
@@ -1,22 +1,26 @@
 require "open-uri"
 
+# Suspect this is never called.  GC July 2023
 module TreeVersionElements
   class DiffHtml
     def initialize(previous_tve, current_tve)
+      throw 'Contact app developer if you see this message (1)'
       @previous_tve = previous_tve
       @current_tve = current_tve
     end
 
     def get
+      throw 'Contact app developer if you see this message (2)'
       url = "#{Rails.configuration.try('services_clientside_root_url')}tree-version/diff-element?e1=#{CGI.escape(@previous_tve)}&e2=#{CGI.escape(@current_tve)}&embed=true"
       Rails.logger.debug("@previous_tve: #{@previous_tve}")
       Rails.logger.debug("@current_tve: #{@current_tve}")
       Rails.logger.debug("TreeVersionElements#DiffHtml get url: #{url}")
-      URI.open(url, "Accept" => "text/html") { |f| f.read }
+      RestClient.get(url, "Accept" => "text/html") { |f| f.read }
     end
 
     # split the diff to get just the after part
     def after_html
+      throw 'Contact app developer if you see this message (3)'
       after = get
       return "nothing found" if after.blank?
 
@@ -25,6 +29,7 @@ module TreeVersionElements
 
     # split the diff to get just the before part
     def before_html
+      throw 'Contact app developer if you see this message (4)'
       before = get
       return "nothing found" if before.blank?
 
