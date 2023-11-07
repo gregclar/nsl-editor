dbff7a80886b152bde4408a874c1fad8587dd659
Greg Clarke <gregbclarke@gmail.com>
2023-11-08 09:04:20 +1100

    NSL-4738: Loader: The any-batch: directive now works even if you have set a default batch.

--- app/controllers/concerns/search/query_defaults.rb
+++ app/controllers/concerns/search/query_defaults.rb
@@ -20,10 +20,12 @@ module Search::QueryDefaults
   def value_already_applied?
     id_regex = /batch-id:/
     name_regex = /batch-name:/
+    any_batch_regex = /any-batch:/
     default_name_regex = /default-batch: #{session[:default_loader_batch_name]}/i
     params[:query_string] =~ id_regex ||
       params[:query_string] =~ name_regex ||
-      params[:query_string] =~ default_name_regex
+      params[:query_string] =~ default_name_regex ||
+      params[:query_string] =~ any_batch_regex
   end
 
   def apply_default_loader_batch
