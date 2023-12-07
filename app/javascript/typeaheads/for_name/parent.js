4c9503e0160f337f7a1d1076a3ced38e61f8a99d
Greg Clarke <gregbclarke@gmail.com>
2023-12-07 13:48:27 +1100

    NSL-4817: Loader: Fix Name parent editing typeahead bug which set family based on parent but only if typeahead was selected, not if typeahead was autocompleted

--- app/javascript/typeaheads/for_name/parent.js
+++ app/javascript/typeaheads/for_name/parent.js
@@ -11,15 +11,16 @@ function setUpNameParentTypeahead() {
               debug('parent typeahead:opened');
             })
             .on('typeahead:selected', function($e,datum) {
-              debug('parent typeahead:selected');
               $('#name_parent_id').val(datum.id);
               $('#name_family_id').val(datum.family_id);
               $("#name-family-typeahead").typeahead('val', datum.family_value);
 
             })
             .on('typeahead:autocompleted', function($e,datum) {
-              debug('parent typeahead:autocompeted');
-                $('#name_parent_id').val(datum.id) })
+              $('#name_parent_id').val(datum.id)
+              $('#name_family_id').val(datum.family_id);
+              $("#name-family-typeahead").typeahead('val', datum.family_value);
+            })
         ;
     };
 }
