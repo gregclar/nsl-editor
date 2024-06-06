ab40e6c5e7af868ef83b591de59e618aea3e70c7
Greg Clarke <gregbclarke@gmail.com>
2024-06-06 16:17:47 +1000

    NSL-5086: Loader: Refresh sort key values when loader records are edited

--- app/models/concerns/loader/name/sort_key.rb
+++ app/models/concerns/loader/name/sort_key.rb
@@ -3,41 +3,42 @@ module Loader::Name::SortKey
 
   def consider_sort_key
     if loader_batch.use_sort_key_for_ordering
-      set_sort_key_if_blank
+      set_sort_key if sort_key.blank?
+      set_sort_key if should_reset_sort_key?
     else
       self.sort_key = nil
     end
   end
 
-  # If a user sets a sort key we want to respect that
-  # so don't over-write it
-  # Conversely, if a user wants a sort_key reset they can simply
-  # blank it out and hit save.
-  def set_sort_key_if_blank
+  def should_reset_sort_key?
+    return false if %w(in-batch-note in-batch-compile-note heading).include? record_type
+    return true if self.changed? && !self.changes_to_save.keys.include?('sort_key')
+    false    
+  end
+
+  def set_sort_key
     normalise_sort_key unless sort_key.blank?
-    if sort_key.blank?
-      case record_type
-      when "accepted"
-        self.sort_key = "#{family.downcase}.family.#{record_type}.#{simple_name.downcase} #{' agenus' if rank == 'genus'}"
-      when "excluded"
-        self.sort_key = "#{family.downcase}.family.#{record_type}.#{simple_name.downcase} #{' agenus' if rank == 'genus'}"
-      when "synonym"
-        self.sort_key = synonym_sort_key(parent.sort_key)
-      when "misapplied"
-        self.sort_key = misapp_sort_key(parent.sort_key)
-      when "heading"
-        self.sort_key = if rank.blank? || rank.downcase == "family"
-                          "#{family.downcase}.family"
-                        else
-                          "aaa-rank-#{rank}-heading"
-                        end
-      when "in-batch-note"
-        self.sort_key = in_batch_note_sort_key if sort_key.blank?
-      when "in-batch-compiler-note"
-        self.sort_key = in_batch_compiler_note_sort_key if sort_key.blank?
-      else
-        self.sort_key = "aaaaaa-unexpected-record-type-#{record_type}"
-      end
+    case record_type
+    when "accepted"
+      self.sort_key = "#{family.downcase}.family.#{record_type}.#{simple_name.downcase} #{' agenus' if rank == 'genus'}"
+    when "excluded"
+      self.sort_key = "#{family.downcase}.family.#{record_type}.#{simple_name.downcase} #{' agenus' if rank == 'genus'}"
+    when "synonym"
+      self.sort_key = synonym_sort_key(parent.sort_key)
+    when "misapplied"
+      self.sort_key = misapp_sort_key(parent.sort_key)
+    when "heading"
+      self.sort_key = if rank.blank? || rank.downcase == "family"
+                        "#{family.downcase}.family"
+                      else
+                        "aaa-rank-#{rank}-heading"
+                      end
+    when "in-batch-note"
+      self.sort_key = in_batch_note_sort_key
+    when "in-batch-compiler-note"
+      self.sort_key = in_batch_compiler_note_sort_key
+    else
+      self.sort_key = "aaaaaa-unexpected-record-type-#{record_type}"
     end
   rescue StandardError => e
     puts e
