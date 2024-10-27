1b2082f2a06040e993c3b7a8f4308f30020ab889
iamgeeeee <contact@gerdadecio.com>
2024-10-28 10:53:07 +1100

    Update permissions for foa edit
    Only allow access to foa editor to users
    assigned to the foa group

--- test/controllers/instances/tabs/authorisation/for_foa/show_foa_tab_test.rb
+++ test/controllers/instances/tabs/authorisation/for_foa/show_foa_tab_test.rb
@@ -27,22 +27,68 @@ class InstanceForFoaShowMostTabsTest < ActionController::TestCase
     @product_item_config = product_item_config(:ecology_pic)
     @profile_item = profile_item(:ecology_pi)
     @request.headers["Accept"] = "application/javascript"
-  end
 
-  test "should show detail and FOA tab links if foa editor requests details tab" do
     get(:show,
         params: { id: @instance.id,
                   tab: "tab_foa_profile" },
         session: { username: "fred",
                    user_full_name: "Fred Jones",
-                   groups: ["edit","foa"] })
-    
+                   groups: ["foa"] })
+  end
+
+  def asserts
+    asserts1
+    asserts2
+    asserts3
+    asserts4
+    asserts5
+  end
+
+  def asserts1
+    assert_response :success
     assert_select "a#instance-foa-profile-tab",
                    /FOA/
                    "Should not show 'FOA Profile' tab link"
-     assert_select "h4", 
+    assert_select "h4",
                    @product_item_config.display_html,
                    "Should show the product item config display_html"
   end
+
+  def asserts2
+    assert_select "li.active a#instance-show-tab",
+                  /Details/,
+                  "Shows 'Details' tab link."
+  end
+
+  def asserts3
+    assert_select "a#instance-edit-tab",
+                  false,
+                  "Does not show 'Edit' tab link."
+    assert_select "a#instance-edit-notes-tab",
+                  false,
+                  "Does not show 'Notes' tab link."
+  end
+
+  def asserts4
+    assert_select "a#instance-cite-this-instance-tab",
+                  false,
+                  "Does not show 'Syn' tab link."
+    assert_select "a#unpublished-citation-tab",
+                  false,
+                  "Does not show 'Unpub' tab link."
+    assert_select "a#instance-apc-placement-tab",
+                  false,
+                  "Should not show 'APC' tab link."
+  end
+
+  def asserts5
+    assert_select "a#instance-comments-tab",
+                  false,
+                  "Does not show 'Adnot' tab link."
+    assert_select "a#instance-copy-to-new-reference-tab",
+                  false,
+                  "Should not show 'Copy' tab link."
+  end
+
 
 end
