dac3098de44dc56c9719272f7b0060b41f62e903
iamgeeeee <contact@gerdadecio.com>
2024-10-28 13:06:21 +1100

    WIP move the foa query in controller to a service model Optimise the behavior by only calling it when the foa tab is clicked

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -205,16 +205,13 @@ class InstancesController < ApplicationController
 
   def find_instance
     @instance = Instance.find(params[:id])
-    if Rails.configuration.try('foa_profile_aware')
-      # TODO: Confirm with Greg on how we are going to identify a product from the UI
-      @product = Profile::Product.find_by(name: "FOA") 
-      @product_configs_and_profile_items = find_or_initialize_profile_items(@product.id, @instance.id)
+    if params[:tab] == "tab_foa_profile"
+      @product_configs_and_profile_items, @product = Profile::ProfileItem::DefinedQuery::ProductAndProductItemConfigs.new(@instance).run_query
     end
   rescue ActiveRecord::RecordNotFound
     flash[:alert] = "We could not find the instance."
     redirect_to instances_path
   end
-  
 
   def instance_params
     params.require(:instance).permit(:instance_type,
@@ -262,8 +259,8 @@ class InstancesController < ApplicationController
     if Rails.configuration.try('batch_loader_aware') &&
           can?('loader/names', 'update') &&
           offer_loader_tab?
-        offer << "tab_batch_loader" 
-        offer << "tab_batch_loader_2" 
+        offer << "tab_batch_loader"
+        offer << "tab_batch_loader_2"
     end
     offer
   end
@@ -346,31 +343,5 @@ class InstancesController < ApplicationController
       parent = parent.parent
     end
     @working_draft.tree_version_elements.order(tree_element_id: "asc").first
-  end
-
-  def find_or_initialize_profile_items(product_id, instance_id)
-    # TODO: Optimise this method!! 
-    # We may not even need this if we first create the profile item
-    # before adding profile texts, references, and annotations.
-
-    # Fetch all product item configs with display_html and associated ProfileItems
-    product_item_configs = Profile::ProductItemConfig
-      .where.not(display_html: nil)
-      .where(product_id: product_id)
-      .includes(:profile_items) # Eager load associated profile items
-  
-    # Fetch existing profile items for quick lookup
-    existing_profile_items = Profile::ProfileItem
-      .where(product_item_config_id: product_item_configs.pluck(:id), instance_id: instance_id)
-      .index_by(&:product_item_config_id) # Create a hash for O(1) lookup
-  
-    # Map the product item configs to their associated profile items
-    product_item_configs.map do |product_item_config|
-      # Find or initialize the associated ProfileItem
-      profile_item = existing_profile_items[product_item_config.id] || Profile::ProfileItem.new(product_item_config: product_item_config, instance_id: instance_id)
-  
-      # Return the product item config and associated or newly initialized profile item
-      { product_item_config: product_item_config, profile_item: profile_item }
-    end
   end
 end
