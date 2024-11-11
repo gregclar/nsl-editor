93d12d811c8a81dbac7264bb1fee650a852f80b8
Greg Clarke <gregbclarke@gmail.com>
2024-11-12 09:26:20 +1100

    Added some fixtures for loader batch tables.
    Added fixture files for:
    
      * Batch_Review
      * Batch_Review_Period
      * Batch_Review_Role
      * Batch_Reviewer
      * Org
      * Users
    
    Also changed:
    
      * structure.db - removed the `not null` constraint on several foreign key cols to allow fixtures to load.
                       Not ideal, but better than trying to order the loading of all (?) fixtures.
    
      * test_helper - re-instated `fixtures :all` because not sure why it was commented out

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -52,6 +52,7 @@ class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  # fixtures %w[users groups memberships]
 
   # Add more helper methods to be used by all tests here...
 end
