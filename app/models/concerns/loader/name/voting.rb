167021f20719b12dfb9281667e4643092eaf83b1
Greg Clarke <gregbclarke@gmail.com>
2024-11-22 15:43:11 +1100

    NSL-5254: Loader: Build out features of batch review voting

--- app/models/concerns/loader/name/voting.rb
+++ app/models/concerns/loader/name/voting.rb
@@ -4,4 +4,10 @@ module Loader::Name::Voting
   def votable?
     ['accepted','excluded'].include?(record_type)
   end
+
+  def summary_of_votes_for_review(review)
+    freq = Hash.new(0)
+    self.name_review_votes.where(batch_review_id: review.id).pluck(:vote).map{|vote| vote ? 'agree' : 'disagree'}.each {|x| freq[x] += 1}
+    freq.map {|key, value| "#{key.capitalize}: #{value}"}.join(',')
+  end
 end
