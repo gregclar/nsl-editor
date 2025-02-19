f730e718a057241ac7d650d5a5c07b55be7a3752
iamgeeeee <contact@gerdadecio.com>
2025-02-20 10:38:19 +1100

    Put back the codes for the product roles in the menu

--- test/factories/role_type.rb
+++ test/factories/role_type.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :role_type, class: "Product::RoleType" do
     description { "Please describe this product role type" }
-    name { "Role Type" }
+    sequence(:name) {|n| "role type #{n}" }
     created_by { "fred" }
     updated_by { "fred" }
   end
