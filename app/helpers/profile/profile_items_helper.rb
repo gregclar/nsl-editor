9455063002e90f2936f831dd50b38603e778c02f
iamgeeeee <contact@gerdadecio.com>
2025-03-19 12:57:25 +1100

    Add an action message in the delete action restriction

--- app/helpers/profile/profile_items_helper.rb
+++ app/helpers/profile/profile_items_helper.rb
@@ -1,11 +1,13 @@
 # Help display profile_item information.
 module Profile::ProfileItemsHelper
-  def sourced_in_profile_items_info(profile_item)
+  def sourced_in_profile_items_info(profile_item, action: :delete)
     return unless profile_item.sourced_in_profile_items.present?
 
     count = profile_item.sourced_in_profile_items.count
+    action_message = ""
+    action_message = "You cannot delete this profile item" if action == :delete
     content_tag(:div, style: "padding: 10px;") do
-      "This item is cited by #{ActionController::Base.helpers.pluralize(count, 'other item')}."
+      "#{action_message}. This item is cited by #{ActionController::Base.helpers.pluralize(count, 'other profile item')}."
     end
   end
 end
