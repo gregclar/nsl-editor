8a4c01732772dbe8e9119560aaca01e34d4987ed
Greg Clarke <gregbclarke@gmail.com>
2025-05-01 09:27:49 +1000

    NSL-5193: Loader Name: Add formatted_text_before and formatted_text_after fields
    Allow editing and display

--- app/helpers/loader/names_helper.rb
+++ app/helpers/loader/names_helper.rb
@@ -15,4 +15,16 @@ module Loader::NamesHelper
   def ref_instance_choice?(loader_name, matching_name)
     loader_name.loader_name_matches.where(name_id: matching_name.id).first.instance_choice_confirmed
   end
+
+  def row_of_formatted_text(text)
+    return '' if text.blank?
+
+    %Q(<tr><td colspan="9"><span class="review loader-name remark-to-reviewers">#{text}</span></td></tr>)
+  end
+
+  def indented_row_of_formatted_text(text, trailing_tags = '')
+    return '' if text.blank?
+
+    %Q(<tr><td width-1-percent"></td><td colspan="9"><span class="review loader-name remark-to-reviewers">#{text}</span></td></tr>#{trailing_tags})
+  end
 end
