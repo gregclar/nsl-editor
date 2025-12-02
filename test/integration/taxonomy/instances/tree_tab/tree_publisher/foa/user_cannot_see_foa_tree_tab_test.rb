83c170c29cff581c50e286dde3db1b430a132069
iamgeeeee <contact@gerdadecio.com>
2025-12-02 14:08:32 +0800

    Update permission messages in tree tab tests to reflect accurate user feedback

--- test/integration/taxonomy/instances/tree_tab/tree_publisher/foa/user_cannot_see_foa_tree_tab_test.rb
+++ test/integration/taxonomy/instances/tree_tab/tree_publisher/foa/user_cannot_see_foa_tree_tab_test.rb
@@ -35,8 +35,8 @@ class TaxoInstanceTreePublisherFoaCannotSeeFoaTreeTab < ActionController::TestCa
                    groups: ["login"],
                    draft: foa_draft})
     assert_response :success, "Tree publisher tab request should be successful"
-    assert_no_match 'data-tab-name="tab_classification" href="#">Tree</a>',
-                  response.body, "Tab Classification should not be in the response"
+    assert_match 'data-tab-name="tab_classification" href="#">Tree</a>',
+                  response.body, "Tab Classification should be in the response"
     assert_no_match '<form', response.body, 'Tab should not contain a form'
   end
 end
