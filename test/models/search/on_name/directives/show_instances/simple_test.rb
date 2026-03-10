b1d597c9a076191ea6be585f3bd9282c5b56bd91
iamgeeeee <contact@gerdadecio.com>
2026-03-10 18:07:13 +1100

    NSL-5729 - Update the minitest test to reflect to new sorting logic

--- test/models/search/on_name/directives/show_instances/simple_test.rb
+++ test/models/search/on_name/directives/show_instances/simple_test.rb
@@ -62,14 +62,14 @@ class SimpleTest < ActiveSupport::TestCase
 
   def check_results_3(search)
     confirm_inst(search, 4, "Metrosideros costata Gaertn.")
-    confirm_disp(search, 5, "instance_as_part_of_concept")
-    confirm_inst(search, 5, "Angophora costata (Gaertn.) Britten")
-    confirm_disp(search, 6, "instance-is-cited-by")
-    confirm_inst(search, 6, "Metrosideros costata Gaertn.")
+    confirm_disp(search, 5, "instance-is-cited-by")
+    confirm_inst(search, 5, "Angophora lanceolata Cav.")
+    confirm_disp(search, 6, "instance_as_part_of_concept")
+    confirm_inst(search, 6, "Angophora costata (Gaertn.) Britten")
     confirm_disp(search, 7, "instance-is-cited-by")
     confirm_inst(search, 7, "Metrosideros costata Gaertn.")
     confirm_disp(search, 8, "instance-is-cited-by")
-    confirm_inst(search, 8, "Angophora lanceolata Cav.")
+    confirm_inst(search, 8, "Metrosideros costata Gaertn.")
   end
 
   def show_results(search)
