13ac5f8b2976c2df12726b87c53667b496511c81
iamgeeeee <contact@gerdadecio.com>
2026-05-19 17:17:37 +1000

    Enhance confirm delete button with spinner and preserve button text

--- app/javascript/confirm_delete_link_click.js
+++ app/javascript/confirm_delete_link_click.js
@@ -3,7 +3,8 @@
     $('body').on('click', 'a.confirm-delete-btn', function() {
       var $btn = $(this);
       $btn.attr('disabled', 'disabled')
-          .html('<i class="fa fa-spinner fa-spin"></i> ' + $btn.text().trim());
+          .prepend(document.createTextNode(' '))
+          .prepend($('<i>').addClass('fa fa-spinner fa-spin'));
       $btn.siblings('.cancel-link').attr('disabled', 'disabled');
     });
   });
