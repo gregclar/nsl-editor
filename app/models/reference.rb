9eea8022e2a7697efdae98589208950971a983dc
Greg Clarke <gregbclarke@gmail.com>
2026-06-25 17:22:02 +1000

    NSL-3918: Reference Delete: Do not offer delete button if comments are attached

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -118,6 +118,10 @@ class Reference < ApplicationRecord
     instances.size.positive?
   end
 
+  def comments?
+    comments.size.positive?
+  end
+
   def not_used_by_any_related_table?
     instances.blank? && profile_item_references.blank?
   end
